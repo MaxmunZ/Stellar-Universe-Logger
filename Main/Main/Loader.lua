a7c80cf4d987b35317fb4f31ac54051366b4087d
MaxmunZ <145468267+MaxmunZ@users.noreply.github.com>
2026-01-27 08:16:58 +0700

    Update Loader.lua

--- Main/Main/Loader.lua
+++ Main/Main/Loader.lua
@@ -1,39 +1,45 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Stellar System | v1.1", HidePremium = false, SaveConfig = true, ConfigFolder = "Stellar"})
+local Window = OrionLib:MakeWindow({Name = "Stellar System | v1.2", HidePremium = false, SaveConfig = true, ConfigFolder = "Stellar"})
 
--- TAB INFO (Persis seperti Chloe X Info)
-local TabInfo = Window:MakeTab({
-	Name = "Info",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+-- Panggil Logika dari Folder Games yang baru Anda buat
+local FishLogic = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaxmunZ/Stellar-Universe-Logger/refs/heads/main/Games/FishIt.lua"))()
 
-TabInfo:AddLabel("Official Stellar Universe Script")
+-- [[ TAB INFO ]]
+local TabInfo = Window:MakeTab({Name = "Info", Icon = "rbxassetid://4483345998"})
+TabInfo:AddLabel("Stellar Universe Official Script")
 TabInfo:AddButton({
-	Name = "COPY DISCORD STELLAR",
+	Name = "Copy Link Discord",
 	Callback = function()
-		setclipboard("https://discord.gg/stellaruniverse") -- Ganti linknya
+		setclipboard("https://discord.gg/stellaruniverse")
 	end    
 })
 
--- TAB WEBHOOK (Persis seperti Chloe X Webhook)
-local TabWeb = Window:MakeTab({
-	Name = "Webhook",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
+-- [[ TAB WEBHOOK (Persis Chloe X) ]]
+local TabWeb = Window:MakeTab({Name = "Webhook", Icon = "rbxassetid://4483345998"})
 TabWeb:AddTextbox({
 	Name = "Webhook Fish Caught",
 	Default = "",
 	TextDisplace = "Tempel Webhook Disini...",
 	Callback = function(Value)
-		_G.FishWebhook = Value -- Menyimpan webhook yang diinput pemain
+		_G.FishWebhook = Value
 	end	
 })
 
-TabWeb:AddButton({
-	Name = "Test Log Webhook",
+-- [[ TAB FISHING ]]
+local TabFish = Window:MakeTab({Name = "Fishing", Icon = "rbxassetid://4483345998"})
+TabFish:AddToggle({
+	Name = "Auto Fishing",
+	Default = false,
+	Callback = function(Value)
+		if Value then
+            FishLogic:StartAutoFarm()
+        else
+            FishLogic:StopAutoFarm()
+        end
+	end    
+})
+
+OrionLib:Init()
 	Callback = function()
         -- Fungsi kirim tes ke discord
         print("Testing to: " .. _G.FishWebhook)
