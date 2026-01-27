f11f825990c5f37b35de1cd13bd701fc9c201f48
MaxmunZ <145468267+MaxmunZ@users.noreply.github.com>
2026-01-27 09:30:40 +0700

    Update Loader.lua

--- Main/Loader.lua
+++ Main/Loader.lua
@@ -3,19 +3,12 @@ repeat task.wait() until game:IsLoaded()
 
 local games = {
     [6701277882] = "https://raw.githubusercontent.com/MaxmunZ/Stellar-Universe-Logger/main/Games/FishIt.lua",
-    -- Anda bisa tambah ID game lain di bawah sini nanti
 }
 
 local scriptURL = games[game.PlaceId]
 
 if scriptURL then
-    -- Memanggil menu utama
     loadstring(game:HttpGet(scriptURL))()
 else
-    -- Notifikasi jika game tidak terdaftar
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "Stellar System",
-        Text = "Game tidak didukung!",
-        Duration = 5
-    })
+    warn("Stellar System: Game ID " .. game.PlaceId .. " belum didukung.")
 end
